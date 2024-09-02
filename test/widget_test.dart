aa5a000410b87b5a20e37c1cf6688a2094c9cb16
Derlis Aguilar <derlishn504@gmail.com>
2024-09-01 21:45:29 -0600

    Initial project configurations and adding packages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:go_triunfo/main.dart';
+import 'package:go_triunfo/app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
